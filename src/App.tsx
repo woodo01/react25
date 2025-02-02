6a135f7ccf45a690e46f48b53cfbe57167eb359e
Ivan Sigaev <ivan.sigaev@check24.de>
2025-02-02 19:43:04 +0100

    feat: add search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,78 @@
 import { Component } from "react";
 import "./App.css";
+import SearchBar from "./components/SearchBar.tsx";
 
-class App extends Component {
+interface Item {
+  uid: string;
+  name: string;
+}
+
+interface Props {}
+
+interface State {
+  items: Item[];
+  error: Error | null;
+}
+
+class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      items: [],
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    const searchTerm = localStorage.getItem("searchTerm") || "";
+    this.fetchItems(searchTerm);
+  }
+
+  fetchItems = (searchTerm: string) => {
+    const url = "https://stapi.co/api/v1/rest/animal/search";
+    const body = new URLSearchParams();
+    if (searchTerm) {
+      body.append("name", searchTerm);
+    }
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: body.toString(),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        return response.json();
+      })
+      .then((data) => this.setState({ items: data.animals }))
+      .catch((error) => this.setState({ error }))
+      .finally(() => this.setState({}));
+  };
+
   render() {
+    const { items, error } = this.state;
+
+    if (error) {
+      throw error;
+    }
+
+    console.log(items);
+
     return (
       <div className="App">
         <div style={{ height: "20%", background: "#f0f0f0" }}>
-          <button onClick={() => {}}>Throw Error</button>
+          <SearchBar onSearch={this.fetchItems} />
+          <button
+            onClick={() => {
+              this.setState({ error: new Error() });
+            }}
+          >
+            Throw Error
+          </button>
         </div>
         <div style={{ height: "80%", overflowY: "scroll" }}></div>
       </div>
